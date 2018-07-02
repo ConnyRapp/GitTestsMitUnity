6f072568089be7027dde2321b1ad6efa350abadf
ConnyRapp <cornelia.rapp@framefield.com>
2018-07-02 16:35:19 +0200

    Add first version of image frames

---
+++ UnityTestDuck/Assets/Test.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+Debug.Log (GetComponent<VideoPlayer>().clip);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+}
